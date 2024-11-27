591d1f02ac71b03beea3d45ddba949bc915ed72e
= <parkernash2001@gmail.com>
2024-11-27 16:48:07 -0600

    lots of changes pt. 2

--- index.js
+++ index.js
@@ -51,34 +51,6 @@ app.use((error, req, res, next) => {
     );
 });
 
-const runPokerGame = () => {
-    const { exec } = require("child_process");
-
-    return new Promise((resolve, reject) => {
-        exec('python3 ./poker/__init__.py', (error, stdout, stderr) => {
-            if (error) {
-                console.error('Exec error:', error);
-                reject(`Error executing python script: ${error.message}`);
-                return;
-            };
-
-            if (stderr) {
-                console.error(`stderr: ${stderr}`);
-            };
-
-            resolve(stdout); // Resolve with the output
-        });
-    });
-};
-
-runPokerGame()
-    .then(result => {
-        console.log(result);
-    })
-    .catch(err => {
-        console.error('Error running poker game:', err);
-    });
-
 // Start
 const PORT = process.env.PORT || 8080;
 db.sequelize.sync().then(() => {
